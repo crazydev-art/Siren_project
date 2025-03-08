b7086e12ecd13d59f07c6dbbd3663d44584c97bd
ubuntu <lionel.matter@outlook.fr>
2025-03-08 21:57:47 +0000

    Redpanda custom container to manage certificate if necessary

--- siren/setenvV2.sh
+++ siren/setenvV2.sh
@@ -1,4 +1,4 @@
-#Création des variables pour les tests en bash
+#Création des variables pour le déploiement container/Redpanda/Prometheus
 export MYDIR=$(pwd)
 export REDPANDA="$MYDIR/Redpanda"
 export FILESIREN="$MYDIR/Files_Siren"
@@ -22,6 +22,16 @@ export MONGO_INITDB_ROOT_USERNAME="root"
 export MONGO_INITDB_ROOT_PASSWORD="root2024"
 export ME_CONFIG_MONGODB_ADMINUSERNAME="root"
 export ME_CONFIG_MONGODB_ADMINPASSWORD="root2024"
+# création des variables pour la mise à jour quotidienne des données SIREN
+export API_KEY="1e82a3c5-03d1-4e96-82a3-c503d12e963d"
+# Yassin export API_KEY = "06871a5f-3654-469d-871a-5f3654869d10"
+export POSTGRES_HOST="postgres"
+# POSTGRES_PORT="5432", Already in line 20
+# POSTGRES_DB="siren", already in line 11
+# POSTGRES_USER="admin", already in line 9
+# POSTGRES_PASSWORD="team@2024" already in line 10
+# PYTHONPATH=/Desktop/DATASCIENTEST/Project_Siren_Siret_data/Siren_project_yass/app
+
 #création des variables pour docker-compose
 echo "MYDIR=$(pwd)" > .env
 echo "REDPANDA=${REDPANDA}" >> .env
@@ -46,6 +56,16 @@ echo "MONGO_INITDB_ROOT_USERNAME=${MONGO_INITDB_ROOT_USERNAME}" >> .env
 echo "MONGO_INITDB_ROOT_PASSWORD=${MONGO_INITDB_ROOT_PASSWORD}" >> .env
 echo "ME_CONFIG_MONGODB_ADMINUSERNAME=${ME_CONFIG_MONGODB_ADMINUSERNAME}" >> .env
 echo "ME_CONFIG_MONGODB_ADMINPASSWORD=${ME_CONFIG_MONGODB_ADMINPASSWORD}" >> .env
+# création des variables pour la mise à jour quotidienne des données SIREN
+echo "API_KEY=${API_KEY}" >> .env
+# Yassin export API_KEY = "06871a5f-3654-469d-871a-5f3654869d10"
+echo "POSTGRES_HOST=${POSTGRES_HOST}" >> .env
+# POSTGRES_PORT="5432", Already in line 20
+# POSTGRES_DB="siren", already in line 11
+# POSTGRES_USER="admin", already in line 9
+# POSTGRES_PASSWORD="team@2024" already in line 10
+# PYTHONPATH=/Desktop/DATASCIENTEST/Project_Siren_Siret_data/Siren_project_yass/app
+cp .env ../data_updater_2/app/
+cp .env ./Redpanda_connect
 cp .env ./Redpanda
 cp .env ./UserTable_Join
-#./prometheus_template.sh
